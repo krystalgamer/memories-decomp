bad00881a1fc0d8590323995c7a3f3ffbfb7d035
Unchiga <unchiga18@gmail.com>
2026-09-11 18:00:25 -0700

    Match func_80041068 without register pins

--- src/game/display_object_updates.c
+++ src/game/display_object_updates.c
@@ -9,6 +9,7 @@
 #include "display_object_packet_submit.h"
 #define GRAPHICS_VIEWPORT_IN_DATA
 #include "graphics_frame.h"
+#include "display_object_packet_submit.h"
 #include "display_object_updates.h"
 
 #include "ordering_tables.h"
