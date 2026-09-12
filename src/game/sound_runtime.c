3a136c94b98cf0c377ab1011ad189cb2ee7792b6
Unchiga <unchiga18@gmail.com>
2026-09-11 20:30:30 -0700

    Match SD_UpdateFades without register bindings

--- src/game/sound_runtime.c
+++ src/game/sound_runtime.c
@@ -34,3 +34,71 @@ full:
     return 0;
 }
 
+void SD_UpdateFades(void) {
+    u8 changed;
+    s16 y;
+
+    changed = 0;
+    if (g_SDValue->field_1588 != 0) {
+        g_SDValue->field_1586 += g_SDValue->field_1588;
+        changed = 1;
+        if (g_SDValue->field_1586 <= g_SDValue->field_158A &&
+            g_SDValue->field_1588 < 0) {
+            g_SDValue->field_1588 = 0;
+            g_SDValue->field_1586 = g_SDValue->field_158A;
+        }
+        if (g_SDValue->field_1586 >= g_SDValue->field_158A &&
+            g_SDValue->field_1588 > 0) {
+            g_SDValue->field_1588 = 0;
+            g_SDValue->field_1586 = g_SDValue->field_158A;
+        }
+    }
+    if (g_SDValue->field_1582 != 0) {
+        g_SDValue->field_1580 += g_SDValue->field_1582;
+        changed = 1;
+        if (g_SDValue->field_1580 <= g_SDValue->field_1584 &&
+            g_SDValue->field_1582 < 0) {
+            g_SDValue->field_1582 = 0;
+            g_SDValue->field_1580 = g_SDValue->field_1584;
+        }
+        if (g_SDValue->field_1580 >= g_SDValue->field_1584 &&
+            g_SDValue->field_1582 > 0) {
+            g_SDValue->field_1582 = 0;
+            g_SDValue->field_1580 = g_SDValue->field_1584;
+        }
+    }
+    if (changed) {
+        u16 v;
+
+        v = g_SDValue->field_0044;
+        if (v) {
+            v = (u16)(v * (g_SDValue->field_1580 + 1));
+            v >>= 8;
+        } else {
+            v = 0;
+        }
+        if (v) {
+            v = (u16)(v * (g_SDValue->field_1586 + 1));
+            v >>= 8;
+        } else {
+            v = 0;
+        }
+        y = v;
+        func_80049F10(y, y);
+    }
+    if (g_SDValue->field_0512 != 0) {
+        g_SDValue->cd_volume += g_SDValue->field_0512;
+        if (g_SDValue->cd_volume <= g_SDValue->field_0049 &&
+            g_SDValue->field_0512 < 0) {
+            g_SDValue->field_0512 = 0;
+            g_SDValue->cd_volume = g_SDValue->field_0049;
+        }
+        if (g_SDValue->cd_volume >= g_SDValue->field_0049 &&
+            g_SDValue->field_0512 > 0) {
+            g_SDValue->field_0512 = 0;
+            g_SDValue->cd_volume = g_SDValue->field_0049;
+        }
+        y = g_SDValue->cd_volume;
+        func_80044DC0(y);
+    }
+}
