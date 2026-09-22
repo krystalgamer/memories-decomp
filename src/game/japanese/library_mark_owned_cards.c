9a35e8f9f34905f1a430239f0a57b128cd3fcf8b
Jose Silva <krystalgamer@users.noreply.github.com>
2026-09-23 01:13:00 +0200

    Match Japanese library owned-card pass (#5872)

---
+++ src/game/japanese/library_mark_owned_cards.c
@@ -0,0 +1,6 @@
+#include "../../types.h"
+
+#define VERSION_JAPAN
+#define VERSION_JAPAN_LIBRARY_MARK_OWNED_CARDS
+#define gLibrary_abCardChest gJapanese_LibraryCardChest
+#include "../library_runtime.c"
