080e3b9803b457cb8f855fcc66d3adf19ee58a34
Jose Silva <krystalgamer@users.noreply.github.com>
2026-09-23 01:33:08 +0200

    Match Japanese options layout update (#5877)

---
+++ src/game/japanese/options_update_layout.c
@@ -0,0 +1,9 @@
+#include "../../types.h"
+
+#define VERSION_JAPAN
+#define VERSION_JAPAN_OPTIONS_UPDATE_LAYOUT
+#define D_8009AF5C gJapanese_OptionsLayoutPositions
+#define gOptions_bOutputType gJapanese_OptionsOutputType
+#define D_8009B380 gJapanese_OptionsOutputCursor
+#define D_8009B388 gJapanese_OptionsSelectionCursor
+#include "../options_screen.c"
