a394fa18f7e346e381d79d033dbd341477519474
Jose Silva <krystalgamer@users.noreply.github.com>
2026-09-22 11:49:21 +0200

    Match Japanese display fade release (#5783)
    Share DisplayObjectFade_ReleaseChannel with the Japanese build.

---
+++ src/game/japanese/display_object_fade_release_channel.c
@@ -0,0 +1,6 @@
+#include "../../types.h"
+
+#define VERSION_JAPAN
+#define VERSION_JAPAN_DISPLAY_FADE_RELEASE_CHANNEL
+#define D_800EAF08 gJapanese_DuelEffectOccupancy
+#include "../display_object_fade_helpers.c"
