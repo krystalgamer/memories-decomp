467c80116c9ba8746e40000518f6b3e514be7912
Jose Silva <krystalgamer@users.noreply.github.com>
2026-09-25 14:00:38 +0200

    Match Japanese companion stream offset command (#6077)
    Reuse the companion shared stream-offset handler with Japanese regional bindings.

---
+++ src/game/japanese/func_80038100.c
@@ -0,0 +1,7 @@
+#include "../../types.h"
+
+#define VERSION_JAPAN
+#define VERSION_JAPAN_FUNC_80038A44
+#define DUEL_EFFECT_STREAM_HIGH_MASK 0x801F0000
+#define func_80038A44 func_80038100
+#include "../duel_effect_object_commands.c"
