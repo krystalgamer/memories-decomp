fd55cf1fbea2defa83d7d737db202c904d17534d
Jose Silva <krystalgamer@users.noreply.github.com>
2026-09-25 06:01:50 +0200

    Match Japanese script image transfer callback
    Refs #5551

---
+++ src/game/japanese/script_image_transfer_callback.c
@@ -0,0 +1,7 @@
+#include "../../types.h"
+
+#define VERSION_JAPAN
+#define VERSION_JAPAN_SCRIPT_IMAGE_TRANSFER_CALLBACK
+#define ScriptImage_TransferCallback func_8002DBE0
+#define D_8009B0F4 gJapanese_FileTransferFlags
+#include "../script_image_objects.c"
