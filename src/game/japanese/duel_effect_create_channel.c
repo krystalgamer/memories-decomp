a7f68a1dbd9602d562971317d4f2782764ad8b30
Jose Silva <krystalgamer@users.noreply.github.com>
2026-09-25 02:17:13 +0200

    Match Japanese DuelEffect_CreateChannel
    Refs #5551

---
+++ src/game/japanese/duel_effect_create_channel.c
@@ -0,0 +1,6 @@
+#include "../../types.h"
+
+#define DIALOG_CHOICE_ADDRESS 0x8009B23D
+#define D_8009AF74 gJapanese_D_8009AF74
+#define D_800EF6EA gJapanese_D_800EF6EA
+#include "../duel_effect_create_channel.c"
